b8748e057cef4417a571389aee335057dafb81ba
jordan819 <patryk.zaucha00@gmail.com>
2023-02-11 18:16:31 +0100

    add icons on HomeScreen

--- src/jvmMain/kotlin/ui/feature/home/HomeScreen.kt
+++ src/jvmMain/kotlin/ui/feature/home/HomeScreen.kt
@@ -1,14 +1,14 @@
 package ui.feature.home
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import io.realm.Database
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -20,32 +20,84 @@ fun HomeScreen(
     navigateToClimberList: () -> Unit,
     database: Database,
 ) {
-    MaterialTheme {
-        Column(
-            Modifier.fillMaxSize(),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center,
+//    MaterialTheme(
+//        colors = Colors(
+//            primary = Color(0, 139, 207, 0),
+//            primaryVariant = Color(0, 139, 207, 0),
+//            secondary = Color(0, 139, 207, 0),
+//            secondaryVariant = Color(0, 139, 207, 0),
+//            background = Color(0, 139, 207, 0),
+//            surface = Color(0, 139, 207, 0),
+//            error = Color(0, 139, 207, 0),
+//            onPrimary = Color(0, 139, 207, 0),
+//            onSecondary = Color(0, 139, 207, 0),
+//            onBackground = Color(0, 139, 207, 0),
+//            onSurface = Color(0, 139, 207, 0),
+//            onError = Color(0, 139, 207, 0),
+//            isLight = true,
+//        )
+//    ) {
+        Row(
+            Modifier.fillMaxSize(),//.background(Color(0, 139, 207, 100)),
+            horizontalArrangement = Arrangement.SpaceEvenly,
+            verticalAlignment = Alignment.CenterVertically,
         ) {
-            Button(onClick = {
-                navigateToClimberList()
-            }) {
-                Text("Zawodnicy")
-            }
-            // TODO: probably remove the following buttons
-            Button(onClick = {
-                CoroutineScope(Dispatchers.IO).launch {
-                    Scraper(database).fetchEvents()
+            IconButton(
+                onClick = { navigateToClimberList() },
+            ) {
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Icon(
+                        painter = painterResource("climbers.svg"),
+                        contentDescription = null,
+                        modifier = Modifier.height(100.dp),
+                    )
+                    Spacer(Modifier.height(10.dp))
+                    Text("Lista zawodników")
                 }
-            }) {
-                Text("Pobierz eventy")
             }
-            Button(onClick = {
-                CoroutineScope(Dispatchers.IO).launch {
-                    Scraper(database).fetchAllClimbers()
+
+            IconButton(
+                onClick = {
+                    CoroutineScope(Dispatchers.IO).launch {
+                        Scraper(database).fetchEvents()
+                    }
+                },
+            ) {
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Icon(
+                        painter = painterResource("events.svg"),
+                        contentDescription = null,
+                        modifier = Modifier.height(100.dp),
+                    )
+                    Spacer(Modifier.height(10.dp))
+                    Text("Pobierz eventy")
                 }
-            }) {
-                Text("Pobierz zawodników")
             }
+
+            IconButton(
+                onClick = {
+                    CoroutineScope(Dispatchers.IO).launch {
+                        Scraper(database).fetchAllClimbers()
+                    }
+                },
+            ) {
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Icon(
+                        painter = painterResource("climbers-fetch.svg"),
+                        contentDescription = null,
+                        modifier = Modifier.height(100.dp),
+                    )
+                    Spacer(Modifier.height(10.dp))
+                    Text("Pobierz zawodników")
+                }
+            }
+
         }
-    }
+//    }
 }
